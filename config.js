fb65544728ba6efaadd90bbb255629a58e449444
Patrick Andrews <patrick@telvue.com>
2015-09-11 22:48:43 -0400

    missed configs

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  db: {
+    production: 'mongodb://typester_client:typester_client@ds049888.mongolab.com:49888/typester',
+    development: 'mongodb://localhost/typester_db'
+  }
+}
